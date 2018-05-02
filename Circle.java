001a10293ad8a58b904f5724b80c4bf77297719f
Mostafa Rizk <m.rizk.student@gmail.com>
2018-05-01 19:49:42 -0600

    Created necessary file for project

---
+++ Circle.java
@@ -0,0 +1,14 @@
+public class Circle {
+   public Circle(double r) {
+      radius = r;
+   }
+   public double area() {
+      double a = Math.PI * radius * radius;
+      return a;
+   }
+   public double circumference() {
+      double c = 2 * Math.PI * radius;
+      return c;
+   }
+   public double radius;
+}
